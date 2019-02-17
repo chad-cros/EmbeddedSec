5d8a4cc1d89e63180bd27e030a7112cec0422f49
chad-cros <chdcrsb777@gmail.com>
2019-02-16 20:30:15 -0600

    added comments

--- fblock.v
+++ fblock.v
@@ -2,13 +2,14 @@
 // constructs f block of DES encryption
 
 module fblock (Rout, Rin, r_key);
-	input Rin[31:0];
-	input r_key [47:0];
+	input Rin[31:0];	//Input of 32 bits from prev. round's rightmost bits
+	input r_key [47:0];	//Key to be XORed with prev. round's leftmost bits
 	
-	output reg Rout [31:0];
-	wire intermediate1 [47:0];
-	wire intermediate2 [47:0];
-	wire intermediate3 [31:0];
+	output Rout [31:0];	//Round output
+
+	wire intermediate1 [47:0];	//48 bits from 32 bits via Expansion P
+	wire intermediate2 [47:0];	//48 bit round key XORed with Expansion P out
+	wire intermediate3 [31:0];	//32 bits from 48 bit output of XOR
 	
 	eP expansionP(
 		.out(intermediate1),
