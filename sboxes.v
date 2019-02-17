4018152c7f80214cfc00ba94e90cefe57117bf1e
m-culler <meganculler333@gmail.com>
2019-02-17 13:47:55 -0600

    Finished S-boxes

--- sboxes.v
+++ sboxes.v
@@ -1304,4 +1304,8 @@ module sboxes(out, in);
 		endcase
 	endcase
 
+	
+	assign out = {out1, out2, out3, out4, out5, out6, out7, out8, out9, out10, out11, out12, out13, out14, out15, out16};
+	
+	
 endmodule
