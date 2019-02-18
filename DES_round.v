5b324fd46e672b345ee80de199086e3fcd53154e
m-culler <meganculler333@gmail.com>
2019-02-18 17:22:39 -0600

    fixed endian-ness

--- DES_round.v
+++ DES_round.v
@@ -13,9 +13,9 @@ module DES_round (round_out, round_in, round_key);
 	wire [31:0] fout; //output of f block
 	
 	assign LeftBits = round_in[31:0];
-	assign RightBits = round_in[63:0];
+	assign RightBits = round_in[63:32];
 	
-	assign round_out[63:32] = RightBits; //wire right half directly to left half of output
+	assign round_out[31:0] = RightBits; //wire right half directly to left half of output
 	
 	//call f block
 	fblock fblock(
@@ -24,7 +24,7 @@ module DES_round (round_out, round_in, round_key);
 		.r_key (round_key)
 	);
 	
-	assign round_out[31:0] =  fout ^ LeftBits; // right half of output is XORed of f block output and Left 32 bits of input
+	assign round_out[63:32] =  fout ^ LeftBits; // right half of output is XORed of f block output and Left 32 bits of input
 
 
 endmodule
