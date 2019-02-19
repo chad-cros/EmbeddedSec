ae39a35750f195fc6511f5e2e2ca680829c352bc
m-culler <meganculler333@gmail.com>
2019-02-18 18:10:34 -0600

    fixed sboxes, swapped inputs to s box

--- fblock.v
+++ fblock.v
@@ -1,5 +1,7 @@
 // fblock.v
 // constructs f block of DES encryption
+`include "EndianSwap48.v"
+`include "EndianSwap32.v"
 
 module fblock (Rout, Rin, r_key);
 	input [31:0] Rin;	//Input of 32 bits from prev. round's rightmost bits
@@ -9,7 +11,9 @@ module fblock (Rout, Rin, r_key);
 
 	wire [47:0] intermediate1 ;	//48 bits from 32 bits via Expansion P
 	wire [47:0] intermediate2;	//48 bit round key XORed with Expansion P out
+	wire [47:0] intermediate22; //flip bits of intermediate 2
 	wire [31:0] intermediate3;	//32 bits from 48 bit output of XOR
+	wire [31:0] intermediate33; //flip bits of intermediate3
 	
 	expansionP ep(
 		.out(intermediate1),
@@ -18,14 +22,24 @@ module fblock (Rout, Rin, r_key);
 	
 	assign intermediate2 = intermediate1 ^ r_key;
 	
+	endianSwap48 es (
+		.swappedOutput(intermediate22),
+		.originalInput(intermediate2)	
+	);
+	
 	sboxes sbox(
 		.out(intermediate3),
-		.in(intermediate2)
+		.in(intermediate22)
+	);
+	
+	endianSwap32 es2 (
+		.swappedOutput(intermediate33),
+		.originalInput(intermediate3)
 	);
 	
 	straightP sp(
 		.out(Rout),
-		.in(intermediate3)
+		.in(intermediate33)
 	);
 
 endmodule
--- sboxes.v
+++ sboxes.v
@@ -4,7 +4,7 @@
 module sboxes(out, in);
 	input [47:0] in;
 	output [31:0] out;
-	
+		
 	wire [5:0] in1;
 	wire [5:0] in2;
 	wire [5:0] in3;
@@ -23,14 +23,14 @@ module sboxes(out, in);
 	reg [3:0] out7;
 	reg [3:0] out8;
 	
-	assign in1 = in[5:0];
-	assign in2 = in[11:6];
-	assign in3 = in[17:12];
-	assign in4 = in[23:18];
-	assign in5 = in[29:24];
-	assign in6 = in[35:30];
-	assign in7 = in[41:36];
-	assign in8 = in[47:42];
+	assign in1 = in[47:42];
+	assign in2 = in[41:36];
+	assign in3 = in[35:30];
+	assign in4 = in[29:24];
+	assign in5 = in[23:18];
+	assign in6 = in[17:12];
+	assign in7 = in[11:6];
+	assign in8 = in[5:0];
 	
 always @(in) begin
 //Sbox 1-------------------------------------------------
